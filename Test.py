e6284ec114e01bb390de021e8190b83141665779
Rupali-Paliwal-07 <99395967+Rupali-Paliwal-07@users.noreply.github.com>
2025-09-04 17:02:26 -0400

    Delete Test.py

--- Test.py
+++
@@ -1,6 +0,0 @@
-try:
-    with open("test_file.txt", "w") as test_file:
-        test_file.write("This is a test.")
-    print("Test file created successfully.")
-except IOError as e:
-    print(f"Error creating test file: {e}")
